d6e2d0e2035a54e77e543ebe5dfec162d1a7bae2
Foluso Kayode <folusolibrary@gmail.com>
2022-02-10 01:39:38 +0100

    setup and fetch fake api data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+//Components
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+//Styles
+import { Wrapper } from './App.styles'
 
-function App() {
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number 
+}
+
+// const getProducts = async (): Promise<CartItemType> => await (await fetch('https://fakestoreapi.com/products')).json();
+
+async function getProducts(){
+  let result = await fetch('https://fakestoreapi.com/products')
+  return result.json()
+}
+
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+
+  console.log(data)
+
+  const getTotalItems = () => null
+  const handleAddToCart = () => null
+
+  if (isLoading) return <LinearProgress />
+  if (error) return <div>Something went wrong...</div>
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Start
     </div>
   );
 }
